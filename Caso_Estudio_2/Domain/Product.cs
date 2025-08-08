af7deb915228d82b69b8afd7f48db57105bf9435
Keylor Chacon <keylorchs@gmail.com>
2025-08-07 19:05:24 -0600

    Cambio de estructura inicial

---
+++ Caso_Estudio_2/Domain/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    internal class Product
+    {
+        [Key]
+        public int IdProduct { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ Caso_Estudio_2/Servicios/Products/IProductServicio.cs
@@ -0,0 +1,12 @@
+﻿using Modelos;
+using System.Collections.Generic;
+
+namespace Servicios.Products
+{
+    public interface IProductServicio
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProduct(int id);
+        void CreateProduct(Product product);
+    }
+}
