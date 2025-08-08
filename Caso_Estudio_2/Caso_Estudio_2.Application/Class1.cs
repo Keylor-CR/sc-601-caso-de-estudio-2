e781eea8c87271e4d77d5843d9c3a45ce37dc779
Keylor Chacon <keylorchs@gmail.com>
2025-08-07 18:49:13 -0600

    Base structure

---
+++ Caso_Estudio_2/Caso_Estudio_2.Application/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caso_Estudio_2.Application
+{
+    public class Class1
+    {
+    }
+}
